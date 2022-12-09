d9e3ca81c2c59181870ee2768bea2dbeed4decaa
54ndro18 <Sandro@10.32.46.16>
2022-12-09 10:25:52 +0100

    created AccountManager

--- ChatApp/src/main/java/com/controller/AccountManager.java
+++ ChatApp/src/main/java/com/controller/AccountManager.java
@@ -1,7 +1,50 @@
 package main.java.com.controller;
 
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
+
+import main.java.com.model.User;
+
+/**
+ * 
+ * @author Sandro
+ * @author sarah
+ *
+ */
 public class AccountManager {
+	
+	// The AccountManager is a singleton
+	private static final AccountManager accountManager = new AccountManager();
+	// Mapping of IPs and their associated users
+	private static Map<InetAddress, User> accountsMap;
+	
 	public AccountManager() {
-		// TODO Auto-generated constructor stub
+		accountsMap = new HashMap<InetAddress, User>();
+	}
+	
+	/**
+	 * 
+	 * @return the AccountManager singleton
+	 */
+	public static AccountManager getInstance() {
+		return accountManager;
+	}
+	
+	/**
+	 * 
+	 * @param user is the User object to add to the accounts map.
+	 */
+	public static void addUser(User user) {
+		accountsMap.put(user.getUserIP(), user);
+	}
+	
+	/**
+	 * 
+	 * @param IP is the IP address associated to the user in question.
+	 * @return the user connected to the ChatApp from IP.
+	 */
+	public static User getUserFromIP(InetAddress IP) {
+		return accountsMap.get(IP);	
 	}
 }
